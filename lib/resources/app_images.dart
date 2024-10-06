5b7624f2d2bde107a47d0425b17c996aebe74911
Alfiya Ahmad <alfiyaahmad@Alfiyas-MacBook-Air.local>
2024-10-06 20:10:29 +0530

    completed first poster

---
+++ lib/resources/app_images.dart
@@ -0,0 +1,3 @@
+const String moviePoster1 = 'assets/images/movie_poster_1.png';
+const String starIcon = 'assets/images/star.svg';
+
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,82 @@
+import 'package:filmfusion/resources/app_colors.dart' as app_colors;
 import 'package:flutter/material.dart';
+import '../resources/app_images.dart' as app_images;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(),
+      body:Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset(app_images.moviePoster1),
+              Text(
+                'Shang chi:Legend of '
+                    '\n the Ten Rings',
+                style: TextStyle(
+                  fontSize:16,
+                  fontWeight: FontWeight.w700,
+                  color: app_colors.sunGlow,
+                ),
+              ),
+              Row(
+               crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Icon(Icons.star,color: app_colors.sunGlow,size: 18,),
+                  const SizedBox(width: 6),
+                  Text(
+                    '4.0 (982)',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 12,
+                      fontStyle: FontStyle.italic,
+                      color: app_colors.gainsBoro,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(Icons.access_time,size: 16,color:app_colors.gainsBoro),
+                  const SizedBox(width: 6),
+                  Text(
+                    '2 hour 5 minutes',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 12,
+                      fontStyle: FontStyle.italic,
+                      color: app_colors.gainsBoro,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(Icons.video_camera_back_outlined, size: 16,color:app_colors.gainsBoro),
+                  const SizedBox(width: 6),
+                  Text(
+                    'Action, Sci-fi',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 12,
+                      fontStyle: FontStyle.italic,
+                      color: app_colors.gainsBoro,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
