91d037e74541fb6a0c47afd63d234feb4159d783
Alfiya Ahmad <alfiyaahmad@Alfiyas-MacBook-Air.local>
2024-10-10 20:15:36 +0530

    completed home screen widget

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -1,3 +1,4 @@
 const String moviePoster1 = 'assets/images/movie_poster_1.png';
 const String starIcon = 'assets/images/star.svg';
+const String moviePoster2 = 'assets/images/movie_poster_2.svg';
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,74 +9,96 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(),
-      body:Row(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Image.asset(app_images.moviePoster1),
-              Text(
-                'Shang chi:Legend of '
-                    '\n the Ten Rings',
-                style: TextStyle(
-                  fontSize:16,
-                  fontWeight: FontWeight.w700,
-                  color: app_colors.sunGlow,
-                ),
-              ),
-              Row(
-               crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Icon(Icons.star,color: app_colors.sunGlow,size: 18,),
-                  const SizedBox(width: 6),
-                  Text(
-                    '4.0 (982)',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      fontSize: 12,
-                      fontStyle: FontStyle.italic,
-                      color: app_colors.gainsBoro,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(Icons.access_time,size: 16,color:app_colors.gainsBoro),
-                  const SizedBox(width: 6),
-                  Text(
-                    '2 hour 5 minutes',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      fontSize: 12,
-                      fontStyle: FontStyle.italic,
-                      color: app_colors.gainsBoro,
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(Icons.video_camera_back_outlined, size: 16,color:app_colors.gainsBoro),
-                  const SizedBox(width: 6),
-                  Text(
-                    'Action, Sci-fi',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      fontSize: 12,
-                      fontStyle: FontStyle.italic,
-                      color: app_colors.gainsBoro,
-                    ),
-                  )
-                ],
-              )
-            ],
+      appBar: AppBar(backgroundColor: Colors.black,),
+      body: const PosterWidget(poster: app_images.moviePoster1, title: 'Shang chi:Legend of '
+          '\n the Ten Rings', rating:'4.0 (982)', runtime: '2 hour 5 minutes', genre: 'Action, Sci-fi'),
+    );
+  }
+}
+
+class PosterWidget extends StatelessWidget {
+  const PosterWidget({
+    required this.poster,
+    required this.title,
+    required this.rating,
+    required this.runtime,
+    required this.genre,
+    super.key});
+
+  final String poster;
+  final String title;
+  final String rating;
+  final String runtime;
+  final String genre;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Image.asset(poster),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+            color: app_colors.sunGlow,
           ),
-        ],
-      ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.star,
+              color: app_colors.sunGlow,
+              size: 18,
+            ),
+            const SizedBox(width: 6),
+            Text(
+              rating,
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 12,
+                fontStyle: FontStyle.italic,
+                color: app_colors.gainsBoro,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.access_time, size: 16, color: app_colors.gainsBoro),
+            const SizedBox(width: 6),
+            Text(
+              runtime,
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 12,
+                fontStyle: FontStyle.italic,
+                color: app_colors.gainsBoro,
+              ),
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.video_camera_back_outlined, size: 16, color: app_colors.gainsBoro),
+            const SizedBox(width: 6),
+            Text(
+              textAlign:TextAlign.left,
+              genre,
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 12,
+                fontStyle: FontStyle.italic,
+                color: app_colors.gainsBoro,
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
