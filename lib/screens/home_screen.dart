4b70b6d89fd1876f2a44ea536e0daad3e87bcf29
Alfiya Ahmad <alfiyaahmad@Alfiyas-MacBook-Air.local>
2024-10-17 19:37:25 +0530

    added: linter rules

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,12 @@
 import 'package:filmfusion/resources/app_colors.dart' as app_colors;
+import 'package:filmfusion/resources/app_images.dart' as app_images;
 import 'package:flutter/material.dart';
-import '../resources/app_images.dart' as app_images;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(final BuildContext context) => Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
@@ -21,8 +20,7 @@ class HomeScreen extends StatelessWidget {
         ),
         itemCount: 10,
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        itemBuilder: (BuildContext context, int index) {
-          return const Padding(
+        itemBuilder: (final BuildContext context, final int index) => const Padding(
             padding: EdgeInsets.all(8.0),
             child: PosterWidget(
               poster: app_images.moviePoster1,
@@ -31,11 +29,9 @@ class HomeScreen extends StatelessWidget {
               runtime: '2 hour 5 minutes',
               genre: 'Action, Sci-fi',
             ),
-          );
-        },
+          ),
       ),
     );
-  }
 }
 
 class PosterWidget extends StatelessWidget {
@@ -48,9 +44,7 @@ class PosterWidget extends StatelessWidget {
   final String genre;
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
+  Widget build(final BuildContext context) => Column(
       children: [
         Image.asset(poster),
         Text(
@@ -62,7 +56,6 @@ class PosterWidget extends StatelessWidget {
           ),
         ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Icon(
               Icons.star,
@@ -82,7 +75,6 @@ class PosterWidget extends StatelessWidget {
           ],
         ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Icon(Icons.access_time, size: 16, color: app_colors.gainsBoro),
             const SizedBox(width: 6),
@@ -95,11 +87,10 @@ class PosterWidget extends StatelessWidget {
                 color: app_colors.gainsBoro,
               ),
               textAlign: TextAlign.center,
-            )
+            ),
           ],
         ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Icon(Icons.video_camera_back_outlined, size: 16, color: app_colors.gainsBoro),
             const SizedBox(width: 6),
@@ -112,10 +103,9 @@ class PosterWidget extends StatelessWidget {
                 fontStyle: FontStyle.italic,
                 color: app_colors.gainsBoro,
               ),
-            )
+            ),
           ],
-        )
+        ),
       ],
     );
-  }
 }
